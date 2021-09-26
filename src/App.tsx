063b0b08e839f9ccfcc7745e8838f906854fcad1
Anderson <anderson.consultant.it@outlook.com>
2021-09-26 14:25:44 -0400

    feature: moved modal to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+
 import { useState } from "react";
 import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 // For accessibility reasons we must bind the Modal class to the root element of the page
@@ -22,9 +24,7 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
-      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
-          <h2>Cadastrar Transação</h2>
-      </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />
     </>
   );
