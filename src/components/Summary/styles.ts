62f24bb32bebf36473bfeb40b8100dcf757fe6d2
Anderson <anderson.consultant.it@outlook.com>
2021-09-25 23:05:02 -0400

    feature: created 3 cards for income, outcome and total with styles

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+    margin-top: -10rem;
+
+    div {
+        background: var(--shape);
+        padding: 1.5rem;
+        border-radius: 0.25rem;
+        color: var(--text-title);
+
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        &.highlight-summary {
+            background: var(--green);
+            color: #fff;
+        }
+
+        strong {
+            margin-top: 1rem;
+            font-size: 2rem;
+            font-weight: 500;
+            display: block;
+        }
+    }
 `;
