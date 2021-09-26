cc944b2c087349aa0bc4f676e5cd8547b95cf610
Anderson <anderson.consultant.it@outlook.com>
2021-09-26 16:52:58 -0400

    feature: added title, value and category to the state

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,6 +22,9 @@ enum TransactionColorEnum {
 
 export function NewTransactionModal({ isOpen, onRequestClose }:INewTransactionModalProps) {
     const [type, setType] = useState('deposit');
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
@@ -44,9 +47,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }:INewTransactionMo
                 <Container onSubmit={handleCreateNewTransaction}>
                     <h2>Cadastrar Transação</h2>
 
-                    <input placeholder="Título"/>
+                    <input 
+                        placeholder="Título" 
+                        value={title} 
+                        onChange={event => setTitle(event.target.value)}
+                    />
 
-                    <input type="number" placeholder="Valor"/>
+                    <input 
+                        type="number" 
+                        placeholder="Valor"
+                        value={value} 
+                        onChange={event => setValue(Number(event.target.value))}
+                    />
 
                     <TransactionTypeContainer>
                         <RadioBoxTransactionType
@@ -70,7 +82,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }:INewTransactionMo
                         </RadioBoxTransactionType>
                     </TransactionTypeContainer>
 
-                    <input placeholder="Categoria"/>
+                    <input 
+                        placeholder="Categoria"
+                        value={category} 
+                        onChange={event => setCategory(event.target.value)}
+                    />
 
                     <button type="submit">
                         Cadastrar
